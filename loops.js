c9f21a0e0f794b0a1b5815e9ea02363a1801b51c
Daniel McClean <dmcclean01@hotmail.com>
2019-05-04 02:50:38 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,38 @@
+//forLoop
+function forLoop(array) {
+  for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`);
+     } 
+     else {
+       array.push(`I am ${i} strange loops.`);
+     }
+  }
+  
+return array;
+  
+}
+
+//whileLoop
+function whileLoop(x) {
+  let countdown = x;
+  
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return "done";
+}
+
+//doWhileLoop
+function doWhileLoop() {
+  var i = 0;
+  
+  function incrementVariable(i) {
+    i = i + 1;
+    return i;
+  }
+
+   do {
+    console.log("I run once regardless.");
+   } while (incrementVariable(i) < 5);
+}
